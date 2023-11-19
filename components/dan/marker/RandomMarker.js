42a22e49d5921900ba3c268042152d1d31ba26c6
Thế Nghĩa <nghiask2018@gmail.com>
2023-11-19 18:13:26 +0700

    Geocoding random position

--- components/dan/marker/RandomMarker.js
+++ components/dan/marker/RandomMarker.js
@@ -1,25 +1,15 @@
 
-import RandomPopup from '/components/dan/popup/RandomPopup.js';
 
-export default function RandomMarker(map) {
+export default function RandomMarker(map, data) {
     const mk = document.createElement('div');
     mk.className = `marker random-marker`;
+    mk.id = `random-marker`;
 
     mk.innerHTML = `
         <img src="/assets/dan/pin.png" alt="pin"/>
     `
 
     var marker = new mapboxgl.Marker(mk)
-        .setLngLat([106.686030, 10.761564])
+        .setLngLat([data.longitude, data.latitude])
         .addTo(map);
-
-
-    // Add Event Handler
-    document.querySelector(`.random-marker`).onclick = function () {
-
-        document.querySelector('.random-popup-root').innerHTML = RandomPopup();
-
-    }
-
 }
-// Hem 341 Nguyen Trai, Phuong Nguyen Cu Trinh, Quan 1, TP HCM
