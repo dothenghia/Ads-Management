4eea7468150fa4bce94da87298df973b2373b0f9
Kizark <kiznlh@gmail.com>
2023-12-01 09:37:02 +0700

    comment passport for later use

--- index.js
+++ index.js
@@ -10,11 +10,7 @@
 
 // các thư mục đều đã có file mẫu nên dựa theo đó để làm
 const express = require('express'); //Khai báo các thứ cần thiết
-const session = require('express-session');
 const expressHbs = require('express-handlebars');
-const passport = require('passport');
-
-app.use(passport.initialize());
 const helpers = {
     "reportTrans": require("./functions/so/translateReportType"),
     "reportLocation": require("./functions/so/getReportLocation"),
@@ -24,7 +20,7 @@ const helpers = {
     "httpFuncs": require("./functions/canbo/httpFuncs")
 }
 const app = express();
-
+app.use(passport.initialize());
 app.use(express.static(__dirname + "/html"));
 //! điều này sẽ khiến khi import các CSS ở trong cái hbs Thì chỉ cần ghi css/....
 app.use(express.json())
@@ -63,19 +59,6 @@ app.engine('hbs', expressHbs.engine({
 }));
 app.set('view engine', 'hbs');
 
-// Use the express-session middleware for session management
-app.use(session({
-    secret: 'suffering',
-    resave: false,
-    saveUninitialized: true,
-}));
-//! KO BỎ COMMENT DÒNG NÀY 
-//app.use(authMiddleware);
-
-
-//Phần bên trên ko nên đụng vào 
-// Simulated user database
-
 //! get sẽ là method mà bọn mày sử dụng nhiều nhất, chỉ khi submit form như đăng nhập đổi mật khẩu thì thì mới xài post
 // Route: Login Page
 app.use('/',require("./routes/general/loginRoute"));
@@ -83,44 +66,30 @@ app.use('/login', require("./routes/general/loginRoute")); //! nếu muốn sử
 app.use('/resetPassword', require("./routes/general/resetPasswordRoute"))
 app.use('/forgotPassword', require("./routes/general/forgotPasswordRoute"))
 app.use('/OTPValidate',require("./routes/general/OTPValidateRoute"))
-app.post("/create", async (req, res) => {
-    try {
-      const id = req.body.email; // Change res.body to req.body
+// app.post("/create", async (req, res) => {
+//     try {
+//       const id = req.body.email; // Change res.body to req.body
   
-      const userJson = {
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-      };
+//       const userJson = {
+//         email: req.body.email,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//       };
   
-      // Wait for the Firestore operation to complete
-      await db.collection("accounts").doc(id).set(userJson);
+//       // Wait for the Firestore operation to complete
+//       await db.collection("accounts").doc(id).set(userJson);
   
-      res.send("User created successfully");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
-  
-//!ĐIỀU hướng cái này: index sẽ là ROOT, từ đó đi vào phải có ./, ko có nó bị lỗi ko hiểu tại sao
+//       res.send("User created successfully");
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   });
 
 //! Viết code bọn bay tiếp theo dưới này
 app.use('/phuong', require("./routes/user/phuongRoute"));
 
 app.use('/so', require("./routes/user/soRoute"))
-//! Mẫu cho việc sử dụng  ROUTES Vào CONTROLLERS
-// app.use("/task1.htm", require("./routes/task1Route"))
-// app.use("/task2.htm", require("./routes/task2Route"))
-// app.use("/task3.htm", require("./routes/task3Route"))
-// app.use("/task4.htm", require("./routes/task4Route"))
-
-//! Sử dụng method get (Bọn bay sẽ làm cái này nhiều nhất)
-//! Sử dụng method post thì ở trên đã có (submit form), lúc submit form thì nhớ điền action = chính page đó (EX: "/login"), method ="post", mỗi thành phần INPUT phải có name (EX: username, password)
-// app.get("/admin",(req,res) => {
-//     res.render("index", {layout: "admin"})
-// })
-
 
 
 //! PORT 3000, đừng thay PORT KHÁC NẾU NHƯ LÀ 1 THẰNG ĐÀN ÔNG
