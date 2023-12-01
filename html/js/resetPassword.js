1a1386568f8e7fdcffe6fabb3259a88a263ea82a
Thế Nghĩa <nghiask2018@gmail.com>
2023-12-01 12:37:13 +0700

    Restyle Reset password screen

--- html/js/resetPassword.js
+++ html/js/resetPassword.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const passwordInput = document.getElementById('password');
     const confirmPasswordInput = document.getElementById('confirmPassword');
-    confirmPasswordInput.addEventListener('change',function(){
-      if (passwordInput.value != confirmPasswordInput.value){
-          confirmPasswordInput.setCustomValidity('Mật khẩu phải trùng nhau!');
-          confirmPasswordInput.reportValidity();
-      }
-      else{
-          confirmPasswordInput.setCustomValidity('');
-      }
+    confirmPasswordInput.addEventListener('change', function () {
+        if (passwordInput.value != confirmPasswordInput.value) {
+            confirmPasswordInput.setCustomValidity('Mật khẩu phải trùng nhau!');
+            confirmPasswordInput.reportValidity();
+        }
+        else {
+            confirmPasswordInput.setCustomValidity('');
+        }
     })
     const showPasswordCheckbox = document.getElementById('showPassword');
 
@@ -17,5 +17,5 @@ document.addEventListener('DOMContentLoaded', function () {
         passwordInput.type = passwordType;
         confirmPasswordInput.type = passwordType;
     });
-    
+
 });
