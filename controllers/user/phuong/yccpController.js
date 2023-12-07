182721f1f33501b68acd6e9f5151b086038d6ae2
KHOA NGUYỄN ANH <21127517@student.hcmus.edu.vn>
2023-12-07 08:34:41 +0700

    Chỉnh trang YCCP và YCDC để lấy data từ Firebase

--- controllers/user/phuong/yccpController.js
+++ controllers/user/phuong/yccpController.js
@@ -1,19 +1,57 @@
 const controller = {}
 const currentPage = 4;
 
-let {PermissionReq} = require("../../../html/assets/request");
-let {Ad, AdLocation} = require("../../../html/assets/data");
+const admin = require("../../../config/firebaseAdmin");
+//https://firebase.google.com/docs/firestore/manage-data/add-data
+const db = admin.firestore();
 
-controller.show = (req, res) => {
-    res.render("partials/screens/phuong/index", {
-        "current": currentPage,
-        "permissionReq": PermissionReq,
-        "ad": Ad,
-        "adLocation": AdLocation,
-        body: function() {
-            return "screens/phuong/yeucaucapphep";
-        }
-    });
+controller.show = async (req, res) => {
+    try {
+        // Get latest snapshot of requested Firebase collections
+        const permissionReqRef = db.collection("permissionReqs");
+        const permissionReqSnapshot = await permissionReqRef.get();
+        const adRef = db.collection("ads");
+        const adSnapshot = await adRef.get();
+        const adLocationRef = db.collection("adLocations");
+        const adLocationSnapshot = await adLocationRef.get();
+        
+        // Extract data from retrieved snapshots
+        const Company = [];
+        let companyId = [];
+        const PermissionReq = [];
+        permissionReqSnapshot.forEach((doc) => {
+            let data = doc.data();
+
+            if (!companyId.includes(data.co.id)) {
+                companyId.push(data.co.id);
+                Company.push(data.co);
+            }
+
+            PermissionReq.push(data);
+        });
+        const Ad = [];
+        adSnapshot.forEach((doc) => {
+            Ad.push(doc.data());
+        });
+        const AdLocation = [];
+        adLocationSnapshot.forEach((doc) => {
+            AdLocation.push(doc.data());
+        });
+
+        res.render("partials/screens/phuong/index", {
+            "current": currentPage,
+            "company": Company,
+            "permissionReq": PermissionReq,
+            "ad": Ad,
+            "adLocation": AdLocation,
+            body: function() {
+                return "screens/phuong/yeucaucapphep";
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 module.exports = controller;
--- controllers/user/phuong/ycdcController.js
+++ controllers/user/phuong/ycdcController.js
@@ -1,18 +1,44 @@
 const controller = {}
 const currentPage = 2;
 
-let {ChangeReq} = require("../../../html/assets/request");
-let {AdLocation} = require("../../../html/assets/data");
+const admin = require("../../../config/firebaseAdmin");
+//https://firebase.google.com/docs/firestore/manage-data/add-data
+const db = admin.firestore();
 
-controller.show = (req, res) => {
-    res.render("partials/screens/phuong/index", {
-        "current": currentPage,
-        "changeReq": ChangeReq,
-        "adLocation": AdLocation,
-        body: function() {
-            return "screens/phuong/yeucaudieuchinh";
-        }
-    });
+controller.show = async (req, res) => {
+    try {
+        // Get latest snapshot of requested Firebase collections
+        const changeReqRef = db.collection("changeReqs");
+        const changeReqSnapshot = await changeReqRef.get();
+        const adLocationRef = db.collection("adLocations");
+        const adLocationSnapshot = await adLocationRef.get();
+        
+        // Extract data from retrieved snapshots
+        // const Company = [];
+        // let companyId = [];
+        const ChangeReq = [];
+        changeReqSnapshot.forEach((doc) => {
+            let data = doc.data();
+
+            ChangeReq.push(data);
+        });
+        const AdLocation = [];
+        adLocationSnapshot.forEach((doc) => {
+            AdLocation.push(doc.data());
+        });
+
+        res.render("partials/screens/phuong/index", {
+            "current": currentPage,
+            "changeReq": ChangeReq,
+            "adLocation": AdLocation,
+            body: function() {
+                return "screens/phuong/yeucaudieuchinh";
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 module.exports = controller;
