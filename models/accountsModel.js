036aeb350ea5182903c440c0859871015c90420b
Kizark <kiznlh@gmail.com>
2023-12-31 12:02:09 +0700

    fix accounts model

--- models/accountsModel.js
+++ models/accountsModel.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose")
 
 const accountsSchema = new mongoose.Schema({
     _id: { type: String, required: true},
-    date: { type: Date, required: true },
+    date: { type: Date },
     area: { type: String },
     role: { type: String },
     phuong_id: { type: String },
     phone: { type: String },
     quan_id: { type: String },
-    email: { type: String, unique: true }, // Make email field unique
+    email: { type: String, unique: true },
     name: { type: String },
-    username: { type: String, unique: true }, // Make username field unique
-    fbID: { type: String },
+    username: { type: String, unique: true }, 
+    fbID: { type: String, unique: true },
     hashedpassword: { type: String },
     microsoftEmail: { type: String },
     delete: { type: Boolean, default: false },
