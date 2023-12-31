6ab2d258b7230b8b9043e1f76f848ef34712fdda
Kizark <kiznlh@gmail.com>
2023-12-31 12:21:11 +0700

    Add thêm các models

---
+++ models/adsModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const adSchema = new mongoose.Schema({
+  adId: { type: Number, required: true },
+  size: { type: String, required: true },
+  reportId: { type: String }, // Assuming reportId is of string type, adjust if needed
+  contractStartDate: { type: Date, required: true },
+  name: { type: String, required: true },
+  contractEndDate: { type: Date, required: true },
+  thumbnails: [{
+    url: { type: String, required: true }
+  }]
+});
+
+const adsModel = mongoose.model('ads', adSchema);
+
+module.exports = adsModel;
