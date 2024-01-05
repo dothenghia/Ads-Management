a5041b46cefeefba38e70ed687d53284f35a4bd1
Thế Nghĩa <nghiask2018@gmail.com>
2024-01-05 14:20:04 +0700

    Thống kê báo cáo theo thời gian

--- testthongkeso/js/thongke.js
+++ testthongkeso/js/thongke.js
@@ -1,122 +1,75 @@
-// ========== Colors
-let tgspColor = '#DC3545';
-let dkndColor = '#F6BA45';
-let dgykColor = '#F448CE';
-let gdtmColor = '#9747FF';
 
-let dangxulyColor = '#FF6400';
-let daxulyColor = '#41AF79';
-let tuchoiColor = '#DC3545';
+import {
+    renderBigTSBCChart,
+    renderMiniTGSPChart,
+    renderMiniDKNDChart,
+    renderMiniDGYKChart,
+    renderMiniGDTMChart
+} from "./renderChart.js";
 
-// ========== Tổng số Báo cáo theo THỜI GIAN
-let tsbcData = {
-    labels: ['TGSP', 'ĐKND', 'ĐGYK', 'GĐTM'],
-    datasets: [{
-        label: 'Số lượng báo cáo',
-        data: [400, 200, 300, 100],
-        backgroundColor: [tgspColor, dkndColor, dgykColor, gdtmColor],
-        hoverOffset: 20
-    }]
-};
+function fetchReportData() {
+    fetch('http://localhost:3000/dan/dsbc')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
 
-var tsbcCanvas = document.getElementById('tsbc-canvas');
+            // @ ========== Tổng số Báo cáo theo THỜI GIAN
+            let tsbcData = { tgsp: 0, dknd: 0, dgyk: 0, gdtm: 0 }
+            data.forEach(item => {
+                if (item.reportForm === "Tố giác sai phạm") tsbcData.tgsp++;
+                if (item.reportForm === "Đăng ký nội dung") tsbcData.dknd++;
+                if (item.reportForm === "Đóng góp ý kiến") tsbcData.dgyk++;
+                if (item.reportForm === "Giải đáp thắc mắc") tsbcData.gdtm++;
+            });
+            renderBigTSBCChart(tsbcData);
 
-var myPieChart = new Chart(tsbcCanvas, {
-    type: 'doughnut', // Type : 'doughnut', 'pie', 'bar', 'line', 'radar', 'polarArea', 'bubble', 'scatter'
-    data: tsbcData, // Data
-    options: {
-        responsive: true,
-        borderWidth: 14,
-        borderColor: 'transparent',
-        borderRadius: 12,
-        spacing: 8,
-        cutout: 110,
-    }
-});
+            // @ ========== Tố giác sai phạm
+            let tgspData = { dangxuly: 0, daxuly: 0, tuchoi: 0 }
+            data.forEach(item => {
+                if (item.reportForm === "Tố giác sai phạm") {
+                    if (item.status === "Đang xử lý") tgspData.dangxuly++;
+                    if (item.status === "Đã xử lý") tgspData.daxuly++;
+                    if (item.status === "Từ chối") tgspData.tuchoi++;
+                }
+            });
+            renderMiniTGSPChart(tgspData);
 
-// @ ========== Tố giác sai phạm
-let tgspData = {
-    datasets: [{
-        data: [100, 200, 50],
-        backgroundColor: [dangxulyColor, daxulyColor, tuchoiColor],
-        hoverOffset: 10
-    }]
-};
-var tgspCanvas = document.getElementById('tsbc-canvas-tgsp');
-var myPieChart = new Chart(tgspCanvas, {
-    type: 'doughnut', // Type : 'doughnut', 'pie', 'bar', 'line', 'radar', 'polarArea', 'bubble', 'scatter'
-    data: tgspData, // Data
-    options: {
-        responsive: true,
-        borderWidth: 10,
-        borderColor: 'transparent',
-        borderRadius: 6,
-        spacing: 4,
-        cutout: 45,
-    }
-});
-// @ ========== Đăng ký nội dung
-let dkndData = {
-    datasets: [{
-        data: [100, 200, 50],
-        backgroundColor: [dangxulyColor, daxulyColor, tuchoiColor],
-        hoverOffset: 10
-    }]
-};
-var dkndCanvas = document.getElementById('tsbc-canvas-dknd');
-var myPieChart = new Chart(dkndCanvas, {
-    type: 'doughnut', // Type : 'doughnut', 'pie', 'bar', 'line', 'radar', 'polarArea', 'bubble', 'scatter'
-    data: dkndData, // Data
-    options: {
-        responsive: true,
-        borderWidth: 10,
-        borderColor: 'transparent',
-        borderRadius: 6,
-        spacing: 4,
-        cutout: 45,
-    }
-});
-// @ ========== Đóng góp ý kiến
-let dgykData = {
-    datasets: [{
-        data: [100, 200, 50],
-        backgroundColor: [dangxulyColor, daxulyColor, tuchoiColor],
-        hoverOffset: 10
-    }]
-};
-var dgykCanvas = document.getElementById('tsbc-canvas-dgyk');
-var myPieChart = new Chart(dgykCanvas, {
-    type: 'doughnut', // Type : 'doughnut', 'pie', 'bar', 'line', 'radar', 'polarArea', 'bubble', 'scatter'
-    data: dgykData, // Data
-    options: {
-        responsive: true,
-        borderWidth: 10,
-        borderColor: 'transparent',
-        borderRadius: 6,
-        spacing: 4,
-        cutout: 45,
-    }
-});
-// @ ========== Giải đáp thắc mắc
-let gdtmData = {
-    datasets: [{
-        data: [100, 200, 50],
-        backgroundColor: [dangxulyColor, daxulyColor, tuchoiColor],
-        hoverOffset: 10
-    }]
-};
-var gdtmCanvas = document.getElementById('tsbc-canvas-gdtm');
-var myPieChart = new Chart(gdtmCanvas, {
-    type: 'doughnut', // Type : 'doughnut', 'pie', 'bar', 'line', 'radar', 'polarArea', 'bubble', 'scatter'
-    data: gdtmData, // Data
-    options: {
-        responsive: true,
-        borderWidth: 10,
-        borderColor: 'transparent',
-        borderRadius: 6,
-        spacing: 4,
-        cutout: 45,
-    }
-});
+            // @ ========== Đăng ký nội dung
+            let dkndData = { dangxuly: 0, daxuly: 0, tuchoi: 0 }
+            data.forEach(item => {
+                if (item.reportForm === "Đăng ký nội dung") {
+                    if (item.status === "Đang xử lý") dkndData.dangxuly++;
+                    if (item.status === "Đã xử lý") dkndData.daxuly++;
+                    if (item.status === "Từ chối") dkndData.tuchoi++;
+                }
+            });
+            renderMiniDKNDChart(dkndData);
+
+            // @ ========== Đóng góp ý kiến
+            let dgykData = { dangxuly: 0, daxuly: 0, tuchoi: 0 }
+            data.forEach(item => {
+                if (item.reportForm === "Đóng góp ý kiến") {
+                    if (item.status === "Đang xử lý") dgykData.dangxuly++;
+                    if (item.status === "Đã xử lý") dgykData.daxuly++;
+                    if (item.status === "Từ chối") dgykData.tuchoi++;
+                }
+            });
+            renderMiniDGYKChart(dgykData);
+
+            // @ ========== Giải đáp thắc mắc
+            let gdtmData = { dangxuly: 0, daxuly: 0, tuchoi: 0 }
+            data.forEach(item => {
+                if (item.reportForm === "Giải đáp thắc mắc") {
+                    if (item.status === "Đang xử lý") gdtmData.dangxuly++;
+                    if (item.status === "Đã xử lý") gdtmData.daxuly++;
+                    if (item.status === "Từ chối") gdtmData.tuchoi++;
+                }
+            });
+            renderMiniGDTMChart(gdtmData);
+
+        })
+}
+
+fetchReportData();
 
 
