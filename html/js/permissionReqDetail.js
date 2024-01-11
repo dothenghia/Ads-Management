2dfcc81d5b0e2b560a4f29b2f5099c6bf9ea330e
Loi-Tap-Lam-Dev <npminhbaobao2@gmail.com>
2024-01-11 16:59:21 +0700

    Update điều chỉnh địa điểm và cấp phép

--- html/js/permissionReqDetail.js
+++ html/js/permissionReqDetail.js
@@ -354,3 +354,16 @@ async function updateAdsInfoData(accountRole, locationId, reportId, size, contra
 
     location.reload();
 }
+
+function generateADId() {
+    // Lấy ngày tháng năm và thời gian hiện tại
+    let currentDate = new Date();
+    let day = currentDate.getDate().toString().padStart(2, '0');
+    let month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Tháng bắt đầu từ 0
+    let year = currentDate.getFullYear().toString();
+    let hours = currentDate.getHours().toString().padStart(2, '0');
+    let minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    let seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    // let milliseconds = currentDate.getMilliseconds();
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
